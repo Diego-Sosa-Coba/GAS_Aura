574962ffc4cb864cc0b5d8154ef28a9581f4a9ef
Diego Sosa-Coba <diego.sosacoba@gmail.com>
2024-03-24 19:50:02 -0700

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Deggo
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
