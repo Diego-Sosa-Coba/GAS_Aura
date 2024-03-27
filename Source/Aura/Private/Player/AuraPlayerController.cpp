214b24ae069c40a57f8636ee2e83aa7b19a2f683
Diego Sosa-Coba <diego.sosacoba@gmail.com>
2024-03-26 19:56:38 -0700

    Code cleanup

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -25,58 +25,16 @@ void AAuraPlayerController::PlayerTick(float DeltaTime)
 
 void AAuraPlayerController::CursorTrace()
 {
-	FHitResult CursorHit;
 	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
 	if (!CursorHit.bBlockingHit) return;
 
 	LastActor = ThisActor;
 	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
 
-	/**
-	* Line trace from cursor. There are several scenarios:
-	*   A. LastActor is null && ThisActor is null
-	*		- Do nothing
-	*	B. LastActor is null && ThisActor is valid
-	*		- Highlight ThisActor
-	*	C. LastActor is valid && ThisActor is null
-	*		- UnHighlight LastActor
-	*	D. Both actors are valid, but LasActor != ThisActor
-	*		- UnHighlight LastActor
-	*		- Highlight ThisActor
-	*	E. Both actors are valid and LastActor == ThisActor
-	*		- Do nothing
-	**/
-
-	if (LastActor == nullptr)
+	if (LastActor != ThisActor)
 	{
-		if (ThisActor != nullptr)
-		{
-			// Case B
-			ThisActor->HighlightActor();
-		}
-		else
-		{
-			// Case A - both are null, do nothing
-		}
-	}
-	else // LastActor is valid
-	{
-		if (ThisActor == nullptr) {
-			// Case C
-			LastActor->UnHighlightActor();
-		}
-		else // both actors are valid
-		{
-			if (LastActor != ThisActor) {
-				// Case D
-				LastActor->UnHighlightActor();
-				ThisActor->HighlightActor();
-			}
-			else
-			{
-				// Case E - do nothing
-			}
-		}
+		if (LastActor) LastActor->UnHighlightActor();
+		if (ThisActor) ThisActor->HighlightActor();
 	}
 }
 
