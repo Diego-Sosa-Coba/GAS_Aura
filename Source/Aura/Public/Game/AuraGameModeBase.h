b6a50214a0e44511a26f4cf9a548317962bdaf1e
Diego Sosa-Coba <diego.sosacoba@gmail.com>
2024-03-31 00:04:41 -0700

    Added Weapon/Spirit to GameMode

--- Source/Aura/Public/Game/AuraGameModeBase.h
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -6,6 +6,8 @@
 #include "GameFramework/GameModeBase.h"
 #include "AuraGameModeBase.generated.h"
 
+class UWeaponClassInfo;
+class USpiritClassInfo;
 class UBossClassInfo;
 /**
  * 
@@ -16,6 +18,12 @@ class AURA_API AAuraGameModeBase : public AGameModeBase
 	GENERATED_BODY()
 
 public:
+	UPROPERTY(EditDefaultsOnly, Category = "Boss Class Defaults")
+	TObjectPtr<UWeaponClassInfo> WeaponClassInfo;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Boss Class Defaults")
+	TObjectPtr<USpiritClassInfo> SpiritClassInfo;
+
 	UPROPERTY(EditDefaultsOnly, Category = "Boss Class Defaults")
 	TObjectPtr<UBossClassInfo> BossClassInfo;
 	
